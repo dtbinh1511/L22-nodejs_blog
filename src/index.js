16d9b3c5bed341a055c7f408f811f204dc2cad65
19130017 <98450194+dtbinh1511@users.noreply.github.com>
2022-08-18 19:54:12 +0700

    add template engine
    express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
+const path = require("path");
 const express = require("express");
-var morgan = require('morgan')
+var morgan = require("morgan");
+var handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+//Template engine: express-handlebars setup
+const hbs = handlebars.engine({ extname: ".hbs" });
+// Rendering engine setup
+app.engine("hbs", hbs);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 //morgan
-app.use(morgan('combined'))
-
+app.use(morgan("combined"));
 
 //route
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.get("/trang-chu", function (req, res) {
+  res.render("home");
+});
+
+app.get("/tin-tuc", function (req, res) {
+  res.render("news");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Connected to port ${port}`);
 });
