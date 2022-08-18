c99ffb4da6c9a07e7d6b82ee7e09562d9dcafc38
19130017 <98450194+dtbinh1511@users.noreply.github.com>
2022-08-18 18:07:24 +0700

    add morgan
    trả lại các phản hồi từ server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+var morgan = require('morgan')
 const app = express();
 const port = 3000;
 
 
+//morgan
+app.use(morgan('combined'))
+
+
 //route
 app.get("/", function (req, res) {
-  res.send("Hello VietNam");
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
