ae946822a5e4d4cad2af415378c5c7b000dcdcef
19130017 <98450194+dtbinh1511@users.noreply.github.com>
2022-08-20 15:20:40 +0700

    add post method
    understand post method difference get method,
    understand `req.query` and `req.body`,
    use library to run `req.body`

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ var handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+// use middleware for express
+app.use(
+  express.urlencoded({
+    extended: true, // use library body-parser
+  })
+); // client -> server: form html
+app.use(express.json()); //client -> server : code javascript json
+
 // use static file
 app.use(express.static("src/public"));
 
@@ -16,15 +24,26 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
 //morgan
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //route
-app.get("/trang-chu", function (req, res) {
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/tin-tuc", function (req, res) {
+app.get("/news", function (req, res) {
   res.render("news");
+});
+app.get("/search", function (req, res) {
+  res.render("search");
+});
+
+app.post("/search", function (req, res) {
+  // UI -> middleware  -> controller
+  // req.query -> middleware
+  // req.body != middleware => use middleware
+  console.log(req.body);
+  res.send("");
 });
 
 app.listen(port, () => {
