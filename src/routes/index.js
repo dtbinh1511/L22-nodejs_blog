bdb289cf8848c98c4e4cc00d90725818d5d4965e
19130017 <98450194+dtbinh1511@users.noreply.github.com>
2022-08-26 22:05:29 +0700

    [CRUD] get detail course to db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const newsRouter = require('./news'); // import news route
 const siteRouter = require('./site'); // import site route
+const coursesRouter = require('./courses'); // import courses route
 
 function route(app) {
-  app.use('/news', newsRouter);
+	app.use('/news', newsRouter);
+	app.use('/courses', coursesRouter);
 
-  app.use('/', siteRouter);
+	app.use('/', siteRouter);
 }
 module.exports = route;
